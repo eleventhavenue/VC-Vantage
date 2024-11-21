ae1a1227515c616bbcd470e1803864079a2efd9a
Tunde Ogunremi <ayot70@gmail.com>
2024-11-21 02:13:55 -0500

    fixed some files

---
+++ types/next-auth.d.ts
@@ -0,0 +1,30 @@
+// types/next-auth.d.ts
+
+import "next-auth";
+
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+  }
+
+  interface User {
+    id: string;
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+    // Add other custom properties here
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    // Add other custom properties here
+  }
+}
